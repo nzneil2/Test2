4d3cc501591c92e0ddb419e40a708d83ee10d760
Gabriel Gauci <gabrielgauci@gmail.com>
2017-12-18 11:05:56 +0100

    first commit of library

---
+++ calculator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    double num1 = 0;
+
+    double num2 = 0;
+
+    int choice;
+
+    printf("Choose from these Functions: \n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n");
+
+    scanf("%lf %d %lf",&num1, &choic, &num2);
+
+}
